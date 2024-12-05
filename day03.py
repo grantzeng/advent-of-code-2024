975f4cf693bf2a682b51569b1ae79a9124321cf4
Grant Zeng <grant.zeng@student.unsw.edu.au>
2024-12-05 18:21:17 +1100

    day03 part2 think about it later

--- day03.py
+++ day03.py
@@ -1,18 +1,22 @@
 import re
 
-
-def evaluate(str): 
+def evaluate(str, pattern=r"mul\((\d+),(\d+)\)"): 
     # This solution only works if there's no nesting of muls... which there doesn't seem to be
-    pattern = r"mul\((\d+),(\d+)\)"
-
     ops = re.findall(pattern, str)
     print(ops)
-    
-    return sum(int(x) * int(y) for x, y, in ops)
+    return sum(int(x) * int(y) for x, y in ops)
+
 
 if __name__ == '__main__':
-    src = './input03.txt'
+    src = './input03a.txt'
     corrupted_data = "".join(line.strip() for line in open(src, 'r').readlines()) 
+
+
     print(evaluate(corrupted_data))
 
+    # ".*?" supposedly results in a non-greedy match
+    # - but this doesn't quite work because of the default of being turned on at start
+    conditional_pattern = r"do\(\).*?mul\((\d+),(\d+)\)"
+    
+    print(evaluate(corrupted_data, pattern=conditional_pattern))
     
