1acbd6acf4b7e3e95dba53bd6e269da38e71b573
Grant Zeng <grant.zeng@student.unsw.edu.au>
2024-12-05 16:39:09 +1100

    numpy-ify day01 soln

--- day01.py
+++ day01.py
@@ -1,24 +1,26 @@
 from collections import Counter 
+import numpy as np 
 
-def day01_part01(l1, l2):
-    # Basically calculate L1 norm of difference (of sorted) vector 
-    return sum(abs(x - y) for x, y in zip(l1, l2))
+def l1_norm(v):
+    return np.linalg.norm(v, 1)
 
-
-def day01_part02(l1, l2): 
-    freq_table = Counter(l2)
-    return sum(x * Counter(l2).get(x, 0) for x in l1) 
+def similarity(v0, v1): 
+    freq_table = Counter(v1)
+    return sum(x * freq_table.get(x, 0) for x in v0) 
 
 if __name__ == '__main__':
 
     src = './input01.txt'
 
-    l1, l2 = [], [] 
+    v0, v1 = [], [] 
     with open(src, 'r') as f: 
         for line in f.readlines():
-            n, m = line.split()
-            l1.append(int(n)); l2.append(int(m))
-            l1 = sorted(l1); l2 = sorted(l2)
+            x0, x1 = line.split()
+            v0.append(int(x0)); v1.append(int(x1))
+    
+    sorted_vector = lambda v: np.sort(np.array(v))
 
-    print(day01_part01(l1, l2))
-    print(day01_part02(l1, l2))
+    v0, v1 = sorted_vector(v0), sorted_vector(v1) 
+
+    print(l1_norm(v0 - v1))
+    print(similarity(v0, v1))
